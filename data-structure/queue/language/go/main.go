c2fa2d0ad3c1836a5fdda446a757604a5d70b9f4
encodi <encodari@gmail.com>
2023-11-30 15:46:00 -0600

    feat: Improved Go stack/queue with generics

--- data-structure/queue/language/go/main.go
+++ data-structure/queue/language/go/main.go
@@ -2,23 +2,58 @@ package main
 
 import "fmt"
 
-type Queue struct {
-	items []interface{}
+type Queue[T any] struct {
+	items []T
 }
 
-func (q *Queue) Enqueue(item interface{}) {
+func (q *Queue[T]) Enqueue(item T) {
 	q.items = append(q.items, item)
 }
 
-func (q *Queue) Dequeue() interface{} {
+func (q *Queue[T]) Dequeue() T {
+	var zero T
 	if len(q.items) == 0 {
-		return nil
+		return zero
 	}
 	item := q.items[0]
 	q.items = q.items[1:]
 	return item
 }
 
-func (q *Queue) IsEmpty() bool {
+func (q *Queue[T]) IsEmpty() bool {
 	return len(q.items) == 0
+}
+
+func (q *Queue[T]) Len() int {
+	return len(q.items)
+}
+
+func (q *Queue[T]) Peek() T {
+	var zero T
+	if len(q.items) == 0 {
+		return zero
+	}
+	return q.items[0]
+}
+
+func (q *Queue[T]) Clear() {
+	q.items = []T{}
+}
+
+func (q *Queue[T]) ToString() string {
+	return fmt.Sprintf("Queue: %v", q.items)
+}
+
+func main() {
+	queue := Queue[int]{}
+	queue.Enqueue(1)
+	queue.Enqueue(2)
+	queue.Enqueue(3)
+	fmt.Println(queue.ToString())
+	fmt.Println(queue.Dequeue())
+	fmt.Println(queue.ToString())
+	fmt.Println(queue.Peek())
+	fmt.Println(queue.Len())
+	fmt.Println(queue.IsEmpty())
+	queue.Clear()
 }
--- data-structure/stack/language/language/go/main.go
+++ data-structure/stack/language/language/go/main.go
@@ -2,23 +2,58 @@ package main
 
 import "fmt"
 
-type Stack struct {
-	items []interface{}
+type Stack[T any] struct {
+	items []T
 }
 
-func (s *Stack) Push(item interface{}) {
+func (s *Stack[T]) Push(item T) {
 	s.items = append(s.items, item)
 }
 
-func (s *Stack) Pop() interface{} {
+func (s *Stack[T]) Pop() T {
+	var zero T
 	if len(s.items) == 0 {
-		return nil
+		return zero
 	}
 	item := s.items[len(s.items)-1]
 	s.items = s.items[:len(s.items)-1]
 	return item
 }
 
-func (s *Stack) IsEmpty() bool {
+func (s *Stack[T]) IsEmpty() bool {
 	return len(s.items) == 0
+}
+
+func (s *Stack[T]) Len() int {
+	return len(s.items)
+}
+
+func (s *Stack[T]) Peek() T {
+	var zero T
+	if len(s.items) == 0 {
+		return zero
+	}
+	return s.items[len(s.items)-1]
+}
+
+func (s *Stack[T]) Clear() {
+	s.items = []T{}
+}
+
+func (s *Stack[T]) ToString() string {
+	return fmt.Sprintf("Stack: %v", s.items)
+}
+
+func main() {
+	stack := Stack[int]{}
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	fmt.Println(stack.ToString())
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.ToString())
+	fmt.Println(stack.Peek())
+	fmt.Println(stack.Len())
+	fmt.Println(stack.IsEmpty())
+	stack.Clear()
 }
